2954514ef610a7cfa5f5effb86bb2ecba0dd9b2e
Matthew Lawson <mmlawson@ucdavis.edu>
2020-07-07 18:45:50 +0200

    switched to dask

--- lineshape.py
+++ lineshape.py
@@ -3,31 +3,43 @@ import time
 import multiprocessing
 import numpy as np
 from scipy.signal import welch
+from dask.distributed import as_completed
+from dask.distributed import Client
+from dask_jobqueue import SLURMCluster
 
 
+def main(n=100, processes=64, days=1):
+    # Set up cluster
+    cluster = SLURMCluster(queue='regular',
+                           project='tassle',
+                           cores=processes,
+                           memory="1 TB"
+                           )
+    cluster.adapt(maximum_jobs=processes - 1)
+
+    client = Client(cluster)
 
 
-def main(n=100, processes=None, days=1):
     # initialize results array
     print("initializing with practice run")
     t_start = time.time()
     f, fft, psd = job(days)
     psd_f, psd_m = psd
-    if processes is None:
-        # account for hyperthreading, we are NOT IO bound
-        processes = multiprocessing.cpu_count() // 2
 
-    p = multiprocessing.Pool(processes)
-    for i in range(n):
-        t = time.time()
-        print("run number ", i)
-        results = p.map(job, days * np.ones(processes))
-        for j in results:
-            fft += j[1]
-            psd_m += j[2][1]
-        print("finished in ", time.time() - t, " seconds")
-    psd_m = psd_m / (n * processes)
-    fft = fft / (n * processes)
+    # if processes is None:
+    #     # account for hyperthreading, we are NOT IO bound
+    #     processes = multiprocessing.cpu_count() // 2
+    t = time.time()
+    result_num = 0
+    futures = client.map(job, days * np.ones(n))    
+    for future, result in as_completed(futures, with_results=True):
+        result_num += 1
+        fft += result[1]
+        psd_m += result[2][1]
+        print("finished job ", result_num, " at ", time.time() - t, " seconds")
+    # when all results are gathered and summed, do the averaging
+    psd_m = psd_m / n
+    fft = fft / n
     print("finished all in ", time.time() - t_start, "seconds")
 
     np.savez_compressed("lineshape_results.npz", f=f, fft=fft,
